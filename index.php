2b8cd567e104ad4b586d0fe6654e3068d318ef0d
SennaDevNL <sennaterpstra@gmail.com>
2020-05-27 16:32:24 +0200

    27-5

--- index.php
+++ index.php
@@ -15,45 +15,69 @@
 </head>
 <body>
     <!-- Navigation -->
-    <nav class="navbar sticky-top navbar-expand-lg navbar-dark" id="main-nav">
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark" id="main-nav">
         <a class="navbar-brand" href="#">Portfolio</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ml-auto">
-                <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="#home">Home</a></li>
                 <li class="nav-item"><a class="nav-link" href="#link1">Over mij</a></li>
                 <li class="nav-item"><a class="nav-link" href="#link2">Projecten</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">Currucilum Vitae</a></li>
+                <li class="nav-item"><a class="nav-link" href="#link3">Currucilum Vitae</a></li>
             </ul>
         </div>
-        </nav>
+    </nav>
     <!-- Masthead -->
     <header class="masthead">
-        <div class="container h-100">
+        <div class="container h-100" id="home">
             <div class="row h-100 align-items-center">
                 <div class="col-12 text-center">
                     <h1 class="font-weight-light text-white display-1">Portfolio.</h1>
-                    <!--<p class="lead text-white">By Senna Terpstra</p>-->
-                    <button class="btn btn-light btn-lg"><a class="nav-link" href="#link1">Over mij!</a></button>
+                    <p class="lead text-white">By Senna Terpstra</p>
                 </div>
             </div>
         </div>
     </header>
-
-    <section class="py-5 test1" id="link1">
+<body>
+    <!-- Over mij -->
+    <section class="py-5 test1 bg-light" id="link1">
   <div class="container h-100">
-    <h2 class="font-weight-light">Page Content</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus ab nulla dolorum autem nisi officiis blanditiis voluptatem hic, assumenda aspernatur facere ipsam nemo ratione cumque magnam enim fugiat reprehenderit expedita.</p>
+      <div class="row">
+          <div class="col-6">
+          <h2 class="font-weight-light">Over mij</h2>
+    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui.</p>
+          </div>
+          <div class="col-6">
+
+          </Div>
+      </div>
   </div>
 </section>
-
+<!-- Projecten -->
 <section class="py-5 test2" id="link2">
   <div class="container">
-    <h2 class="font-weight-light">Page Content 2</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus ab nulla dolorum autem nisi officiis blanditiis voluptatem hic, assumenda aspernatur facere ipsam nemo ratione cumque magnam enim fugiat reprehenderit expedita.</p>
-  </div>
+    <h2 class="font-weight-light">Projecten</h2>
+</div>
+</section>
+<!-- CV -->
+<section class="py-5 test2 bg-light" id="link3">
+  <div class="container">
+    <h2 class="font-weight-light">Currucilum Vitae</h2>
+</div>
 </section>
 </body>
+<!-- Footer-->
+<footer class="footer py-4">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-4 text-lg-left">Copyright © Your Website 2020</div>
+                    <div class="col-lg-4 my-3 my-lg-0">
+                        <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a><a class="btn btn-dark btn-social mx-2" href="https://www.facebook.com/senna.terpstra/"><i class="fab fa-facebook-f"></i></a><a class="btn btn-dark btn-social mx-2" href="https://www.instagram.com/sennaterpstra/"><i class="fab fa-instagram"></i></a>
+                    </div>
+                    <div class="col-lg-4 text-lg-left"></div>
+                </div>
+            </div>
+        </footer>
 </html>
